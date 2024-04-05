6bed7c3e734eb911b157085af9be3e07d0f52e62
Roberto Martinez <robertocarlosmartinez.contact@gmail.com>
2024-04-05 17:19:27 -0600

    Agregando personaje y escenerio de juego

---
+++ p-unity-2/Assets/Scripts/Player Movemment.cs
@@ -0,0 +1,61 @@
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float speed;
+    [SerializeField] private LayerMask groundLayer;
+    [SerializeField] private LayerMask wallLayer;
+    private Rigidbody2D body;
+    private Animator anim;
+    private BoxCollider2D boxCollider;
+    private void Awake()
+    {
+        //Toma referencias del Rigidbody y el Animator de nuesrto objeto
+        body = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+        boxCollider = GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        body.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, body.velocity.y);
+
+        //Voltear al personaje hacia la izquierda o la derecha
+        if(horizontalInput > 0.01f)
+            transform.localScale = Vector3.one;
+        else if(horizontalInput < -0.01f)
+            transform.localScale = new Vector3(-1, 1, 1);
+
+        if(Input.GetKey(KeyCode.Space) && isGrounded())
+            Jump();
+
+        //Asignar parametros a Animator
+        anim.SetBool("Run", horizontalInput != 0);
+        anim.SetBool("Grounded", isGrounded());
+
+        print(onWall());
+    }
+
+    private void Jump()
+    {
+        body.velocity = new Vector2(body.velocity.x, speed);
+        anim.SetTrigger("Jump");
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+    }
+
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, Vector2.down, 0.1f, groundLayer);
+        return raycastHit.collider != null;
+    }
+
+    private bool onWall()
+    {
+        RaycastHit2D raycastHit = Physics2D.BoxCast(boxCollider.bounds.center, boxCollider.bounds.size, 0, new Vector2(transform.localScale.x, 0), 0.1f, wallLayer);
+        return raycastHit.collider != null;
+    }
+}
