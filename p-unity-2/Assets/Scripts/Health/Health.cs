2969689c87a59da18c622c40f1617bc6c4b7654a
Roberto Martinez <robertocarlosmartinez.contact@gmail.com>
2024-04-08 09:26:23 -0600

    Agregando trampas enemigas al juego y cambio de scripts

--- p-unity-2/Assets/Scripts/Health/Health.cs
+++ p-unity-2/Assets/Scripts/Health/Health.cs
@@ -1,16 +1,25 @@
+using System.Collections;
 using UnityEngine;
 
 public class Health : MonoBehaviour
 {
+    [Header ("Health")]    
     [SerializeField] private float startingHealth;
     public float currentHealth { get; private set; }
     private Animator anim;
     private bool dead;
 
+    [Header ("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
+
+
     private void Awake()
     {
         currentHealth = startingHealth;
         anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
     public void TakeDamage(float _damage)
     {
@@ -19,7 +28,7 @@ public class Health : MonoBehaviour
         if (currentHealth > 0)
         {
             anim.SetTrigger("Hurt");
-            //iframes
+            StartCoroutine(Invulnerability());
         }
         else
         {
@@ -34,5 +43,17 @@ public class Health : MonoBehaviour
     public void AddHealth(float _value)
     {
         currentHealth = Mathf.Clamp(currentHealth + _value, 0, startingHealth);
+    }
+    private IEnumerator Invulnerability()
+    {
+        Physics2D.IgnoreLayerCollision(10, 11, true);
+        for (int i = 0; i < numberOfFlashes; i++)
+        {
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / numberOfFlashes * 2);
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / numberOfFlashes * 2);
+        }
+        Physics2D.IgnoreLayerCollision(10, 11, false);
     }
 }
